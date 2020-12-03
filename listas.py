bff919eb62724f4334f4529175636f7ca98052c9
nando.devs <nando.devs@gmail.com>
2020-12-03 15:00:07 -0300

    Atualizado

--- listas.py
+++ listas.py
@@ -1,48 +1,54 @@
-lista = [1,11, 30, 3, 5 ,7]
-lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+# lista = [1,11, 30, 3, 5 ,7]
+# lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
+
+lista = [3, 5, 7, 10, 11]
+resultado = []
+for x in lista:
+    if x % 2 == 1:
+        resultado.append(x)
+print(resultado)
+
+# tupla = (1, 10, 12, 14)
+# print(len(tupla))
+# tupla_animal = tuple(lista_animal)
+# print(type(tupla_animal))
+# print(tupla_animal)
+
+# lista_numerica = list(tupla)
+# print(type(lista_numerica))
+# print(lista_numerica)
 
 
-tupla = (1, 10, 12, 14)
-print(len(tupla))
-tupla_animal = tuple(lista_animal)
-print(type(tupla_animal))
-print(tupla_animal)
+# #print(lista_animal[1])
 
-lista_numerica = list(tupla)
-print(type(lista_numerica))
-print(lista_numerica)
-
-
-#print(lista_animal[1])
-
-# lista.sort()
-# lista_animal.sort()
-# print(lista)
-# print(lista_animal)
-# lista_animal.reverse()
-# print(lista_animal)
-
-
-
-# soma = 0
-# for x in lista:
-#     print(x)
-#     soma += x
-# print(soma)
-#print(min(lista_animal))
-
-# nova_lista = lista_animal * 3
-# print(nova_lista)
-
-# if 'lobo' in lista_animal:
-#     print('existe um gato na lista')
-# else:
-#     print('não existe um gato na lista')
-#     lista_animal.append('lobo')
-#     print(lista_animal)
-
-# # lista_animal.pop(0)
+# # lista.sort()
+# # lista_animal.sort()
+# # print(lista)
+# # print(lista_animal)
+# # lista_animal.reverse()
 # # print(lista_animal)
 
-# lista_animal.remove('elefante')
-# print(lista_animal)
+
+
+# # soma = 0
+# # for x in lista:
+# #     print(x)
+# #     soma += x
+# # print(soma)
+# #print(min(lista_animal))
+
+# # nova_lista = lista_animal * 3
+# # print(nova_lista)
+
+# # if 'lobo' in lista_animal:
+# #     print('existe um gato na lista')
+# # else:
+# #     print('não existe um gato na lista')
+# #     lista_animal.append('lobo')
+# #     print(lista_animal)
+
+# # # lista_animal.pop(0)
+# # # print(lista_animal)
+
+# # lista_animal.remove('elefante')
+# # print(lista_animal)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,48 +1,6 @@
-lista = [1,11, 30, 3, 5 ,7]
-lista_animal = ['cachorro', 'gato', 'elefante', 'lobo', 'arara']
-
-
-tupla = (1, 10, 12, 14)
-print(len(tupla))
-tupla_animal = tuple(lista_animal)
-print(type(tupla_animal))
-print(tupla_animal)
-
-lista_numerica = list(tupla)
-print(type(lista_numerica))
-print(lista_numerica)
-
-
-#print(lista_animal[1])
-
-# lista.sort()
-# lista_animal.sort()
-# print(lista)
-# print(lista_animal)
-# lista_animal.reverse()
-# print(lista_animal)
-
-
-
-# soma = 0
-# for x in lista:
-#     print(x)
-#     soma += x
-# print(soma)
-#print(min(lista_animal))
-
-# nova_lista = lista_animal * 3
-# print(nova_lista)
-
-# if 'lobo' in lista_animal:
-#     print('existe um gato na lista')
-# else:
-#     print('não existe um gato na lista')
-#     lista_animal.append('lobo')
-#     print(lista_animal)
-
-# # lista_animal.pop(0)
-# # print(lista_animal)
-
-# lista_animal.remove('elefante')
-# print(lista_animal)
+lista = [3, 5, 7, 10, 11]
+resultado = []
+for x in lista:
+    if x % 2 == 1:
+        resultado.append(x)
+print(resultado)
